2e00a907a718892b493c23fb0002b6aca815b73e
Karsa <wanghongtao114@gmail.com>
2016-03-15 18:17:53 +0800

    Initial Commit

---
+++ Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Karsa wang on 3/15/16.
+//  Copyright © 2016 Karsa wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Karsa wang on 3/15/16.
+//  Copyright © 2016 Karsa wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
